2e4726242e75d336a60cab20de786a19302ec2a3
narugen0101 <narugen0101@gmail.com>
2021-08-31 23:45:03 +0900

    Release to heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,5 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user
-  before_action :ensure_correct_user, {only: [:edit,:update, :destroy]}
   
   def index
     @posts = Post.all.order(created_at: :desc)
